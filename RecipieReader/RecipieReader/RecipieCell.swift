11b8363bcf498dac88b9d0aabc3b68bddb5e79f2
Russell Mirabelli <russell@mirabellifamily.org>
2017-10-03 14:31:30 -0500

    add tableview

---
+++ RecipieReader/RecipieReader/RecipieCell.swift
@@ -0,0 +1,17 @@
+//
+//  RecipieCell.swift
+//  RecipieReader
+//
+//  Created by Russell Mirabelli on 9/28/17.
+//  Copyright © 2017 Russell Mirabelli. All rights reserved.
+//
+
+import UIKit
+
+class RecipieCell: UITableViewCell {
+    
+    @IBOutlet weak var recipieNameLabel: UILabel!
+    
+    
+    
+}
